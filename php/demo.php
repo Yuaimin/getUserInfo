c2069a14de809feb10572da69c6775ce69cc8b8d
于爱民 <766468591@qq.com>
2018-03-26 17:14:27 +0800

    Update demo.php

--- php/demo.php
+++ php/demo.php
@@ -1,15 +1,8 @@
 <?php
 
 header("Content-type: text/html; charset=utf-8");
-// 指定允许其他域名访问  
-header('Access-Control-Allow-Origin:*');
-// 响应类型  
-header('Access-Control-Allow-Methods:POST');
-// 响应头设置  
-header('Access-Control-Allow-Headers:x-requested-with,content-type');
 
 include_once "wxBizDataCrypt.php";
-
 
 $appid = $_GET['appid'];
 $sessionKey = $_GET['session_key'];
